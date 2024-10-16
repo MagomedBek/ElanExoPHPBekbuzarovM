6986fec79b51ddaa174a169a4a2e47ce36180fbf
MBekbuza <32449439+MagomedBek@users.noreply.github.com>
2024-10-16 14:51:14 +0000

    first

--- AlgoPHP2/exo1.php
+++ AlgoPHP2/exo1.php
@@ -4,3 +4,10 @@ caractère passée en argument en majuscules et en rouge.
 Vous devrez appeler la fonction comme suit : convertirMajRouge($texte) ;</p>
 
 <h2>Resultat</h2>
+<?php
+    $text = "Mon texte en rouge";
+
+    function convertirMajRouge($text){
+     mb_strtoupper($text);
+    }
+    echo convertirMajRouge($text);
