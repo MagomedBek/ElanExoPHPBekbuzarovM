68ce1a50aea92318ecfa0d699e3faad4784e1d67
MBekbuza <32449439+MagomedBek@users.noreply.github.com>
2024-11-20 16:54:07 +0100

    Add files via upload

---
+++ hello-world/js/main.js
@@ -0,0 +1,82 @@
+///Log de Hello JS!
+console.log("Hello JS!");
+
+/* 
+Apprenons à coder 
+en JavaScript
+*/
+
+// 1 les instructions
+
+intrction1;
+intrction2;
+
+console.log("Hello JS!");
+let age =37;
+
+
+// 2 Les blocs
+nomBloc(parametrage){
+    // instruction(s) et/ou bloc(s)
+}
+
+if (age >= ageMin){
+
+}
+
+while (k < 10){
+
+}
+
+nom
+BlocdeuxMots
+unPeuPlusDeMots
+longNomDeVariableEnCamelCase
+eteEnsoleille // sans accent, pas "étéEnsoleillé" 
+segmentPoint1
+
+let animal = "dog"; // déclaration et initialisation de la variable animal
+animal ="cat"; // modification de sa valeur
+animal = "bird"; // modification de sa valeur
+
+const deux = 2; // la valeur ne peut pas étre modifiée
+// la ligne suivante suivante est mise en commentare car elle provoque une erreur
+// deux = 3; // -> Uncaught TypeError: Assignement to constant variable.
+
+let repas = "pizza";
+const criDuChien = "aboiment";
+
+let isAdulst = true;
+let isTaskDone = false;
+const hasBeenToTokyo = true ;
+
+{// début du bloc dans lequel age est déclarée
+    let age = 37; // début de sa portée
+    {
+        {
+
+        }
+    } // fin du bloc dans lequel age est déclarée = fin de sa portée
+
+}
+
+let price = 1.17;
+const animal1 = "dog";
+let isAdult = true;
+
+let numberToGuess = 2; // 2
+numberToGuess +=3; // numerToGuess = numberToGuess +3 // 5
+numberToGuess -=2; // numerToGuess = numberToGuess -2 // 3
+numberToGuess ++; // numerToGuess = numberToGuess +1 // 4
+numberToGuess *= 10; // numerToGuess = numberToGuess *10 // 40
+numberToGuess /= 2;// numerToGuess = numberToGuess / 2 // 20
+numberToGuess %=7;// numerToGuess = numberToGuess % 7 // 6
+numberToGuess --;// numerToGuess = numberToGuess - 1 // 5
+
+console.log("numberToGuess [2 +3 -2 +1 *10 /2 %7 -1] =")
+const prenom = "John";
+const nom1 = "Smith";
+const nomComplet = prenom = " " = nom;
+console.log("Je m'appelle " + nomComplet );
+console.log("Je m'appelle  ${nomComplet}," );
+
