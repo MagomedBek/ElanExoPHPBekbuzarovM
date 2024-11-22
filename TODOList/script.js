fb70c691a48a8e504a3f6c17b565d1e2ae5ecb6c
MBekbuza <32449439+MagomedBek@users.noreply.github.com>
2024-11-22 14:59:31 +0100

    Add files via upload

--- TODOList/script.js
+++ TODOList/script.js
@@ -2,8 +2,11 @@ const addBtn = document.querySelector('#btn');
 const taskCard = document.querySelector('.todoCard');
 const taskConteiner = document.querySelector('#todoCards');
 const delBtn = document.querySelector(".delBtn");
+const counter = document.querySelector("#compteur p");
+
 delBtn.addEventListener('click',function(){ // delete default task on click
     deleteTask(taskCard); // target the right task
+    compter();
 });
 
 function addTask (){
@@ -12,19 +15,26 @@ function addTask (){
     const newTaskArea = newTask.querySelector(".task");
     newTaskArea.value = "New Task"; // set new task text to "New Task"
     newDelBtn.addEventListener('click',function(){ // delete default task on click
-        deleteTask(taskCard); 
+        deleteTask(newTask); 
     });
    
     taskConteiner.appendChild(newTask);
+    compter();
    // updeteCount();
 };
 
 
 function deleteTask(task){
     task.remove();// remove the task
+    compter()
 
 };
 
+function compter(){
+  let quantite = taskConteiner.childElementCount;
+  counter.innerHTML= "Le nombre de tasks est :"+quantite;
 
+}
 
 addBtn.addEventListener('click',addTask); // add a task on click
+compter();
