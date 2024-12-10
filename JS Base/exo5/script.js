fc8cd4fbabfc452a212647eec369fe94768c9e42
MBekbuza <32449439+MagomedBek@users.noreply.github.com>
2024-12-10 16:53:28 +0100

    Add files via upload

--- JS Base/exo5/script.js
+++ JS Base/exo5/script.js
@@ -5,13 +5,43 @@ const block = document.querySelector(".block");
 
 
 function generer(){
+    
+    //let favorits =[]
     quotes.forEach(element => {
-        block.innerHTML += "<div class='citation'><p>'"+element.content+"'</p><p>"+element.author+"</p></div>"
-        console.log(element.author);
+        block.innerHTML += "<div class='citation' id='"+element.id+"'><p><i>\""+element.content+"\"</i></p><p><i  class=' visible fa-regular fa-heart fav '></i>"+element.author+"</p></div>"
+       
     });
+    listing ()
     
 }
 
 
+function listing (){
+    let favorits = document.querySelectorAll("p .fav");
+    let favoris =[];
+    //let text = "";
+    favorits.forEach((fav,index) => {
+        fav.addEventListener("click", ()=>{
+           let contenu = JSON.stringify(quotes[index]);
+            if (!favoris.includes(contenu)){
+                fav.classList.replace("fa-regular", "fa-solid")
+                favoris.push(contenu );
+                
+            }else{
+                fav.classList.replace( "fa-solid","fa-regular")
+                favoris.splice(favoris.indexOf(contenu),1);
+            }
+         //text = "id :" quotes[index].id + "\n author : " + quotes[index].author + " \n author :" + quotes[index].title +"\n author :"+ quotes[index].content;
+            localStorage.setItem('fav', favoris);
+            for(let i=0; i<localStorage.length; i++) {
+                let key = localStorage.key(i);
+                alert(`${key}: ${localStorage.getItem(key)} \n`);
+                
+              }
 
+            
+        })
+        
+    })
+}
  generer();
